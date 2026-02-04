b147fe2784e946f03f683f2d509e64c740f4049a
carcasbla-gva <carcasbla@alu.edu.gva.es>
2026-02-04 09:50:25 +0100

    CalculadoraServiceTest Acabado

--- src/test/java/CalculadoraServiceTest.java
+++ src/test/java/CalculadoraServiceTest.java
@@ -13,8 +13,30 @@ public class CalculadoraServiceTest {
         when(mockCalculadora.sumar(3, 2)).thenReturn(5);
         when(mockCalculadora.multiplicar(5, 2)).thenReturn(10);
 
+
+
         CalculadoraService service = new CalculadoraService(mockCalculadora);
         assertEquals(10, service.dobleDeLaSuma(3, 2));
+    }
+
+    @Test
+    public void testRestaAlCuadrado() {
+        Calculadora mockCalculadora = mock(Calculadora.class);
+        when(mockCalculadora.restar(3, 2)).thenReturn(1);
+        when(mockCalculadora.multiplicar(1, 1)).thenReturn(1);
+
+        CalculadoraService service = new CalculadoraService(mockCalculadora);
+        assertEquals(1, service.restaAlCuadrado(3,2));
+    }
+
+    @Test
+    public void testSumaYDivide() {
+        Calculadora mockCalculadora = mock(Calculadora.class);
+        when(mockCalculadora.sumar(4, 2)).thenReturn(6);
+        when(mockCalculadora.dividir(6, 2)).thenReturn(3);
+
+        CalculadoraService service = new CalculadoraService(mockCalculadora);
+        assertEquals(3, service.sumaYDivide(4, 2, 2));
     }
 
 
