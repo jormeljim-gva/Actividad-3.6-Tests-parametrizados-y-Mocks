627e8942de2d28da4c53ec66aeb1481dcfbf2d03
carcasbla-gva <carcasbla@alu.edu.gva.es>
2026-02-04 09:51:24 +0100

    Ejercicio acabado

--- src/main/java/Ejemplo1.java
+++
@@ -1,8 +0,0 @@
-public class Ejemplo1 {
-    public String esAdmitido(int porcentaje){
-        if(porcentaje >= 50 && porcentaje <= 90){
-            return "SI";
-        }
-        return "NO";
-    }
-}
