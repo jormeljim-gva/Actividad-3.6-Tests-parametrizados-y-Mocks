28e238df431d4b82cebf4bab18f2ef2ebcb43cd7
jormeljim-gva <jormeljim@alu.edu.gva.es>
2026-02-04 08:32:41 +0100

    Ejemplo 2 acabado

--- src/test/java/Ejemplo2Test.java
+++ src/test/java/Ejemplo2Test.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,15 +19,16 @@ class Ejemplo2Test {
         ejemplo2 = null;
     }
 
-    @Test
-    void clasesEquivalencia() {
-        assertEquals("SI", ejemplo2.esAdmitido("apto"));
-        assertEquals("NO", ejemplo2.esAdmitido("no apto"));
-        assertThrows(IllegalArgumentException.class, () -> {
-            ejemplo2.esAdmitido("otro");
-        });
-
+    @ParameterizedTest
+    @CsvSource({"apto, SI", "no apto, NO"})
+    void esAdmitido(String apto, String resultado) {
+        assertEquals(resultado, ejemplo2.esAdmitido(apto));
     }
 
+    @ParameterizedTest
+    @ValueSource (strings={"otro", "PP", "hola"})
+    void noEsAdmitido(String apto){
+        assertThrows(IllegalArgumentException.class, () -> ejemplo2.esAdmitido(apto));
+    }
 
 }
